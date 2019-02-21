1875b34f1c67b991b7d144dee0f784365c6dc80b
Trevor Lewis <tenzingtitan@gmail.com>
2019-02-21 15:13:47 -0700

    Climbing Stairs Finished

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -3,7 +3,21 @@
 import sys
 
 def climbing_stairs(n, cache=None):
-  pass 
+  if n < 0:
+    return 0
+  elif n == 0:
+    return 1
+  elif cache is None:
+    cache = [0 for x in range(n + 1)]
+  elif cache[n] is not 0:
+    return cache[n]
+
+
+  total = climbing_stairs(n - 3, cache) + climbing_stairs(n - 2, cache) + climbing_stairs(n - 1, cache)
+  cache[n] = total
+
+  return total
+
 
 
 if __name__ == "__main__":
