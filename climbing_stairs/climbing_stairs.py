274adcc1750877d4018a143781893ba9faa1865b
Trevor Lewis <tenzingtitan@gmail.com>
2019-02-28 15:38:31 -0700

    My attempt at climbing stairs. Need to come back to this.

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,8 +2,15 @@
 
 import sys
 
-def climbing_stairs(n, cache=None):
-  pass
+def climbing_stairs(n):
+    if n <= 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return climbing_stairs(n-1) + climbing_stairs(n-2) + climbing_stairs(n-3)
 
 
 
